5d769a43710a713bac6b35b6cf2cf3af030c2a23
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-06 19:58:57 +0000

    Property Listings By City

--- 1_queries/03_propert_listings_by_city.sql
+++ 1_queries/03_propert_listings_by_city.sql
@@ -2,7 +2,7 @@
 SELECT properties.*, AVG(property_reviews.rating) AS average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_reviews.property_id
--- WHERE city = 'Vancouver' ?!
+-- WHERE city = 'Vancouver' because in seeds it's like : 'North Vancouver'
 WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
 HAVING AVG(property_reviews.rating) >= 4
