16c35ef91d802cbab23251290d8c13a18291512d
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-06 19:59:16 +0000

    All My Reservations

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -6,6 +6,7 @@ FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id 
 WHERE reservations.guest_id = 1 
+-- WHERE properties.owner_id = 1 WRONG because it said: The reservations will be for a single user, so use 1 for the user_id.
 AND reservations.end_date < now()::date
 GROUP BY reservations.id, properties.id
 ORDER BY reservations.start_date
