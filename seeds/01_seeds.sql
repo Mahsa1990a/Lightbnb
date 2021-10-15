c24b92d9d8f7780dedd92c52363e16ea3a64ce99
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-10-15 20:49:35 +0000

    update

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -4,9 +4,9 @@ VALUES ('Eva li', 'eva@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70
 ('Dominic parks', 'dominic@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-VALUES (1, 'Speed lamp', 'desc', 'url', 'url', 800, 2, 2, 1, 'Canada', '651 Hejto Rd', 'Toronto', 'Ontario', 'lll333', true ), 
-(2, 'Blavk corner', 'desc', 'url', 'url', 2000, 3, 2, 3, 'Canada', '179 Hartford', 'Toronto', 'Ontario', 'lll222', true),
-(3, 'Habit Mix', 'desc', 'url', 'url', 3000, 2, 2, 6, 'Canada', '30 Hejto Rd', 'Toronto', 'Ontario', 'lll444', true);
+VALUES (1, 'Speed lamp', 'desc', 'https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg', 'https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 800, 2, 2, 1, 'Canada', '651 Hejto Rd', 'Toronto', 'Ontario', 'lll333', true ), 
+(2, 'Blavk corner', 'desc', 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg', 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 2000, 3, 2, 3, 'Canada', '179 Hartford', 'Toronto', 'Ontario', 'lll222', true),
+(3, 'Habit Mix', 'desc', 'https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg', 'https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 3000, 2, 2, 6, 'Canada', '30 Hejto Rd', 'Toronto', 'Ontario', 'lll444', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2018-12-30', '2019-01-30', 1, 3),
@@ -17,3 +17,4 @@ INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, mes
 VALUES (1, 2, 1, 3, 'msg'),
 (3, 1, 2, 5, 'msg'),
 (2, 3, 3, 6, 'msg');
+
